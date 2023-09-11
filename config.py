597fe88b466252388da89e5029746eb48d672d65
Ninad Kulkarni <78587643+nkulkarni3297@users.noreply.github.com>
2023-09-11 17:10:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 
 SUBDOMAIN = "genai.healtharkinsights.com"
-IP_ADDRESS = "139.59.17.132"
+IP_ADDRESS = "157.245.13.224"
 KEY_FILE = "integrum"
 DEBUG = False  # Set to False in production
